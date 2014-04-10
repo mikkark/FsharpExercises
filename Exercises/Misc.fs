53750e1f1ae72eb1646646554bbb58149aee8df6
mikkark <mikkark@gmail.com>
2014-04-10 07:04:47 +0300

    added lottery number generator, just for the sake of it

---
+++ Exercises/Misc.fs
@@ -0,0 +1,21 @@
+﻿namespace Misc
+
+module LotteryNumbers =
+    //Create 7 lottery numbers and sort them.
+
+    let rand = new System.Random();
+
+    let rec findDistinctNumber (next:int, l:int list) =
+        match List.exists((=) next) l with
+        | false -> next
+        | true  -> findDistinctNumber (rand.Next(32), l)
+
+    let rec getLotteryNumbers (l:int list) =
+        match l.Length with
+        | 7    -> l
+        | _    -> getLotteryNumbers (findDistinctNumber (rand.Next(32), l) :: l)
+
+    let sortedLotteryNumbers = 
+        getLotteryNumbers [] |> List.sort
+
+    printfn "%A" sortedLotteryNumbers
